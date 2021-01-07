21ed8e1a9503e66a23c94358b8d8a845ab8a90ae
ShauryaKishoreMishra <76519572+ShauryaKishoreMishra@users.noreply.github.com>
2021-01-07 16:51:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,22 +5,16 @@ const Bodies=Matter.Bodies;
 var engine,world,box1,ground;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(displayWidth,displayHeight);
 engine=Engine.create();
 world=engine.world;
-var options={restitution:0.9};
-box1=Bodies.rectangle(400,200,70,70,options);
-World.add(world,box1);
-var options={isStatic:true}
-ground=Bodies.rectangle(400,390,800,20,options);
-World.add(world,ground);
-
+box1=new Box(400,200,70,70);
+ground= new Ground(displayWidth/2,displayHeight-10,displayWidth);
 }
 
 function draw() {
   background(0);  
   Engine.update(engine);
-  rectMode(CENTER);
-  rect(box1.position.x,box1.position.y,70,70);
-  rect(ground.position.x,ground.position.y,800,20);
+  box1.display();
+  ground.display();
   }
